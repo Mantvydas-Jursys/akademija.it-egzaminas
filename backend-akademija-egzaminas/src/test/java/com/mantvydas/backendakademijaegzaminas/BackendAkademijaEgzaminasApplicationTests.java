104df02bd80ce45cd1833ab02c4931ad16923479
Mantvydas-Jursys <Mantvydas.jursys@stud.vtmc.lt>
2023-06-27 09:37:25 +0300

    first commit for exam

---
+++ backend-akademija-egzaminas/src/test/java/com/mantvydas/backendakademijaegzaminas/BackendAkademijaEgzaminasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mantvydas.backendakademijaegzaminas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAkademijaEgzaminasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
