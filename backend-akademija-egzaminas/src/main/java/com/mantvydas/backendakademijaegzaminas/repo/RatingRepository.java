771c355c2d7605794aff2b64bd11f2bbc4195c5d
Mantvydas-Jursys <Mantvydas.jursys@stud.vtmc.lt>
2023-06-27 13:23:28 +0300

    final touches

--- backend-akademija-egzaminas/src/main/java/com/mantvydas/backendakademijaegzaminas/repo/RatingRepository.java
+++ backend-akademija-egzaminas/src/main/java/com/mantvydas/backendakademijaegzaminas/repo/RatingRepository.java
@@ -2,8 +2,12 @@ package com.mantvydas.backendakademijaegzaminas.repo;
 
 import com.mantvydas.backendakademijaegzaminas.model.Rating;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 
 public interface RatingRepository extends JpaRepository<Rating, Long> {
+
+    @Query("SELECT AVG(r.ratingValue) FROM Rating r WHERE r.masterId = :masterId")
+    Double getAverageRatingByMasterId(Long masterId);
 
 }
